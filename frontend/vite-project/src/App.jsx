e378328d431c2c6e9e1d1dc64350ce8e9012ecca
vincent munywoki <vincentmunywoki12@gmail.com>
2025-04-08 16:57:23 +0300

    I updated my app.jsx file importing Chat component

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Chat from "./components/Chat";
 function App() {
   return (
     <main>
       <h1 className="font-bold text-blue-500">Hello, world</h1>
+      <Chat/>
     </main>
 
 );
